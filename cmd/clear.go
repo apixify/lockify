10ca7b160cbac3a8e532ef7fa09d5eb569d83158
Ahmed Abdelgawad <ahmed.elkayaty92@gmail.com>
2025-11-13 00:11:34 +0100

    Implement clear cache cmd

--- cmd/clear.go
+++ cmd/clear.go
@@ -1,3 +1,24 @@
 package cmd
 
+import (
+	"fmt"
+
+	"github.com/apixify/lockify/internal/service"
+	"github.com/spf13/cobra"
+)
+
 // lockify cache clear
+var clearCmd = &cobra.Command{
+	Use:   "cache clear",
+	Short: "Clear cached passphrase.",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		fmt.Println("⏳ clearing cached passphrases")
+		service.ClearAllPassphrases()
+		fmt.Println("⏳ cleared cached passphrases")
+		return nil
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(clearCmd)
+}
